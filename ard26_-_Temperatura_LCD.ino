7c02f87bbc1faea6268e273ab5920f73d6d43548
Nomadeinchina <jjosealfaro@gmail.com>
2026-09-08 13:14:42 -0600

    Define LEDs and buzzer for temperature monitoring
    Added new LED and buzzer constants for temperature alerts.

--- ard26_-_Temperatura_LCD.ino
+++ ard26_-_Temperatura_LCD.ino
@@ -7,12 +7,24 @@
 #define DHTTYPE DHT11
 DHT dht (DHTPIN, DHTTYPE);
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-const int ledVerde = 8;   // Temperatura normal
 
-
+const int ledVerde = 8;      // Temperatura normal
+const int ledAmarillo = 9;   // Temperatura alta
+const int ledRojo = 10 ;     // Temperatura crítica
+const int buzzer = 7 ;       // Alarma sonora
 
 void setup() {
-  // put your setup code here, to run once: Sensor de tempetarura
+lcd.begin(16, 2);
+dth.begin();
+  pinMode(ledVerde, OUTPUT);
+  pinMode(ledAmarillo, OUTPUT);
+  pinMode(ledRojoerde, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+//------------Apagar todos al inicio-----------
+  digitalWrite(ledVerde, LOW);
+  digitalWrite(ledAmarillo, LOW);
+  digitalWrite(ledRojoerde, LOW);
+  digitalWrite(buzzer, LOW);
 
 }
 
