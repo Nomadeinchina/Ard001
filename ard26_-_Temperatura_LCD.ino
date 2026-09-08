06ca2cf012974eee2f75ea78b162ea91973136c9
Nomadeinchina <jjosealfaro@gmail.com>
2026-09-08 12:14:01 -0600

    Initialize DHT sensor and LCD in Arduino sketch
    Added DHT and LiquidCrystal libraries, defined constants for DHT sensor and LCD, and initialized objects.

--- ard26_-_Temperatura_LCD.ino
+++ ard26_-_Temperatura_LCD.ino
@@ -1,4 +1,15 @@
-*/sensor _______ + LCD /*
+//sensor _______ + LCD
+#include <DHT.h>
+#include <LiquidCrystal.h>
+
+//---------Definiciones-----------
+#define DHPIN 2
+#define DHTTYPE DHT11
+DHT dht (DHTPIN, DHTTYPE);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int ledVerde = 8;   // Temperatura normal
+
+
 
 void setup() {
   // put your setup code here, to run once: Sensor de tempetarura
