11a5a4a12345f02d6a3f6038595bc8751df16963
Nomadeinchina <jjosealfaro@gmail.com>
2026-09-11 22:09:16 -0600

    Actualizar ard2602_DHT11_Temp&h.ino

--- ard2602_DHT11_Temp&h.ino
+++ ard2602_DHT11_Temp&h.ino
@@ -1,25 +1,27 @@
-// Código completo para medir la temperatura y la humedad con el sensor DHT11.
+// Código completo para medir la temperatura y la humedad con el sensor DHT11
+// + LCD 16x2
 
-#include <DHT.h> // Incluimos librería
-#define DHTPIN 8 // Pin digital donde se conecta el sensor DHT11
-// Dependiendo del tipo de sensor
+#include "DHT.h"                       // Incluimos librería
+#include <LiquidCrystal.h>
+
+#define DHTPIN 8                       // Pin digital donde se conecta el sensor DHT11
+                                       // Dependiendo del tipo de sensor
 #define DHTTYPE DHT11
- 
-// Inicializamos el sensor DHT11
-DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // Pines de LCD en placa
+           
+DHT dht(DHTPIN, DHTTYPE);              // Inicializamos el sensor DHT11
  
 void setup() {
-  // Inicializamos comunicación serie
-  Serial.begin(9600);
- 
-  // Comenzamos el sensor DHT
-  dht.begin();
+lcd.begin(16, 2);                      // Define Formato de LCD (Columnas, Filas)
+Serial.begin(9600);                    // Inicializamos comunicación serie
+
+dht.begin();                           // Comenzamos el sensor DHT
  
 }
  
 void loop() {
-    // Esperamos 5 segundos entre medidas
-  delay(5000);
+   
+  delay(5000);   // Esperamos 5 segundos entre medidas
  
   // Leemos la humedad relativa
   float h = dht.readHumidity();
@@ -33,12 +35,24 @@ void loop() {
     Serial.println("Error obteniendo los datos del sensor DHT11");
     return;
   }
- 
+
+
   // Calcular el índice de calor en Fahreheit
   float hif = dht.computeHeatIndex(f, h);
   // Calcular el índice de calor en grados centígrados
   float hic = dht.computeHeatIndex(t, h, false);
- 
+
+// Datos enviados al monitor LCD
+lcd.setCursor(0,0);
+lcd.print("Temp.: ");
+lcd.print(t);
+lcd.print(" C ");
+lcd.setCursor(0,1);
+lcd.print("Humedad: ");
+lcd.print(h);
+lcd.print(" % ");
+
+// Datos enviados al monitor Serial
   Serial.print("Humedad: ");
   Serial.print(h);
   Serial.print(" %\t");
